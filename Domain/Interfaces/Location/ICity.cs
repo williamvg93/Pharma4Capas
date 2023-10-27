35ffa88cff9ede84a44ae83ec8b59b30f58e343a
williamvg93 <sonico_5071@hotmail.com>
2023-10-26 20:36:55 -0500

    feat: :sparkles: Interfaces, Added IGenericRepo and Interfaces for all entities

---
+++ Domain/Interfaces/Location/ICity.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Domain.Entities.Location;
+
+
+namespace Domain.Interfaces.Location;
+
+public interface ICity : IGenericRepository<City>
+{
+
+}
---
+++ Domain/Interfaces/Product/ICProduct.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Domain.Entities.Product;
+
+namespace Domain.Interfaces.Product;
+
+public interface ICProduct : IGenericRepository<CProduct>
+{
+
+}
